4233d5e09372ed9894a99f8c4518937737b7d1c4
Peggun <125943831+Peggun@users.noreply.github.com>
2024-08-18 12:07:21 +0800

    Update run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,6 @@
 import subprocess
 import time
+import sys
 import platform
 
 if platform.system() == 'Windows':
@@ -9,13 +10,16 @@ if platform.system() == 'Windows':
     time.sleep(5)
 
     result = subprocess.run(['pytest'], capture_output=True, text=True)
-
     print(result.stdout)
 
     if result.stderr:
         print(result.stderr)
 
+    if result.returncode != 0:
+        sys.exit(result.returncode)
+
     server_process.terminate()
+    sys.exit()
 
 else:
     server_process = subprocess.Popen(['python', 'run.py'])
@@ -26,4 +30,11 @@ else:
 
     print(result.stdout)
 
+    if result.stderr:
+        print(result.stderr)
+
+    if result.returncode != 0:
+        sys.exit(result.returncode)
+
     server_process.terminate()
+    sys.exit()
